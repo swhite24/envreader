027886ce79a44796c63e2b9db2ea19cce3d01d86
Steven White <steven.white@ensequence.com>
2014-12-23 12:48:31 -0500

    Improve GoDoc formatting

--- envreader.go
+++ envreader.go
@@ -1,21 +1,21 @@
-/*
-	envreader provides a small wrapper for os.GetEnv for retrieving a collection of environment variables into a map.
+// envreader.go
+//
+// (C) Steven White 2014
 
-	Simple example:
-
-		myEnv := envreader.Read("PATH")
-		usePath(myEnv["PATH"])
-*/
+// Package envreader provides a small wrapper for os.GetEnv for retrieving a collection of environment variables into a map.
+//
+// Simple example:
+//
+//   myEnv := envreader.Read("PATH")
+//   usePath(myEnv["PATH"])
 package envreader
 
 import (
 	"os"
 )
 
-/*
-	Reads and returns all environment variables found in keys
-*/
-func Read (keys ...string) map[string]string {
+//Read reads and returns all environment variables found in keys
+func Read(keys ...string) map[string]string {
 	vals := make(map[string]string)
 	for _, key := range keys {
 		vals[key] = os.Getenv(key)
