89a8492c211bfb564a69ef4451f7d2dc25045d88
Steven White <steven.white@ensequence.com>
2014-12-23 12:48:58 -0500

    Improve tests

--- envreader_test.go
+++ envreader_test.go
@@ -1,20 +1,35 @@
 package envreader
 
 import (
+	"os"
 	"testing"
 )
 
-var keys = []string{ "PATH", "USER" }
+var (
+	keys   = []string{"ENVREADER1", "ENVREADER2"}
+	values = []string{"foo1", "foo2"}
+)
 
-func TestRead (t *testing.T) {
+func TestRead(t *testing.T) {
+	setup()
 	vals := Read(keys...)
 	if len(vals) != len(keys) {
 		t.Errorf("Incorrect number of fields")
 	}
 
-	for _, key := range keys {
+	for i, key := range keys {
+		val := values[i]
 		if vals[key] == "" {
 			t.Errorf("Missing key: %s\n", key)
 		}
+		if vals[key] != val {
+			t.Errorf("Incorrect key for %s: Got %s, Want %s", key, vals[key], val)
+		}
+	}
+}
+
+func setup() {
+	for i := range keys {
+		os.Setenv(keys[i], values[i])
 	}
 }
